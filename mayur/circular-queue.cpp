fb650eb73f7dca3bbd87d6c991a269c43b14f9e6
Mayur Aitavadekar <40314912+mayuraitavadekar@users.noreply.github.com>
2020-02-13 18:35:11 +0530

    Update circular-queue.cpp

--- mayur/circular-queue.cpp
+++ mayur/circular-queue.cpp
@@ -2,89 +2,143 @@
 
 using namespace std;
 
-// declaring varibales
-int front = -1;
-int rear = -1;
-int arr[100];
-int q_size;
+class circular_queue {
 
-bool enQueue() {
-    
-    if((rear-front==q_size-1) || (rear==front-1)) {
-        cout<<"queue is full"<<endl;
-        return false;
-    }
-    
+    private : 
     int element;
-    cout<<"enter element : "<<endl;
-    cin>>element;
+    int q_size;
+    int *arr;
+    int front;
+    int rear;
 
-    if(front==-1 && rear==-1) {
-        front++;
-        rear++;
-        arr[rear] = element;
-    }
-    else {
-        rear++;
-        arr[rear] = element;
-    }
-}
+    public :
 
-int deQueue() {
-
-    if((front==-1 && rear==-1) || (front>rear)) {
-        front=-1;
-        rear=-1;
-        cout<<"queue is empty."<<endl;
-        return -1;
+    circular_queue(int q_size) {
+        this->q_size = q_size;
+        arr = new int[q_size];
+        front = -1;
+        rear = -1;
     }
 
-    int element;
-    element = arr[front];
-    front++;
-    return element;
-}
+    void enQueue() {
 
-void display() {
-    for(int i=front;i<=rear;i++) {
-        cout<<arr[i]<<"<-";
+        if((front==0 && rear==q_size-1) || rear==(front-1)%(q_size-1)) {
+            cout<<"queue is full."<<endl;
+            return;
+        }
+
+        cout<<"enter element : "<<endl;
+        cin>>element;
+
+        if(front==-1 && rear==-1) {
+            // intial condition
+            front++;
+            rear++;
+            arr[rear] = element;
+        }
+
+        else if(front !=0 && rear == q_size-1) {
+            rear = 0;
+            arr[rear] = element;
+        }
+        
+        else {
+            rear++;
+            arr[rear] = element;
+        }
+
+        cout<<element<<" added."<<endl;
     }
-    cout<<endl;
-}
 
-int main() {
+    int deQueue() {
+        
+        if(front==-1) {
+            return -1;
+        }
+
+        element = arr[front];
+        arr[front] = 0;
+
+        // now what to do with front
+        if(front==q_size-1) {
+            front=-1;
+        }
+
+        else if(front>rear) {
+            front=-1;
+            rear=-1;
+        }
+        else {
+            front++;
+        }
+
+        return element;
+    }   
+
+    void display() {
+        if(front==-1 && rear==-1) cout<<"queue is empty"<<endl;
+
+        if(rear>=front) {
+            
+            for(int i=front;i<=rear;i++) {
+                cout<<arr[i]<<"<-";
+            }
+            cout<<endl;
+        }
+
+        else {
+
+            for(int i=front;i<q_size;i++) {
+                cout<<arr[i]<<"<-";
+            }
+
+            for(int i=0;i<=rear;i++) {
+                cout<<arr[i]<<"<-";
+            }
+            cout<<endl;
+        }
+
+        return;
+    }
+
+};
+
+int main () {
 
     int choice;
-    bool res;
     int element;
-    
-    cout<<"enter size : "<<endl;
-    cin>>q_size;
-    
+    int size;
+    bool res;
+
+    cout<<"enter size of queue : "<<endl;
+    cin>>size;
+
+    circular_queue obj(size);
 
     do {
         cout<<"1. add element : "<<endl;
         cout<<"2. delete element : "<<endl;
-        cout<<"3. display : "<<endl;
+        cout<<"3. display element : "<<endl;
 
         cin>>choice;
 
         switch(choice) {
-            case 1:
-            res = enQueue();
-            if(res!=false) cout<<"added."<<endl;
+            
+            case 1 :
+            obj.enQueue();
             break;
 
-            case 2 : 
-            element = deQueue();
+            case 2 :
+            element = obj.deQueue();
             if(element!=-1) cout<<element<<" removed."<<endl;
+            else cout<<"queue is empty."<<endl;
             break;
 
             case 3 : 
-            display();
+            obj.display();
             break;
         }
-
     }while(choice!=0);
+    
 
 }
