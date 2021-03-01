6ff5b9a68917b32fb8a3beebc91800d6a4cd2d5c
Mayur Aitavadekar <40314912+mayuraitavadekar@users.noreply.github.com>
2021-03-01 20:30:45 +0530

    Update BinarySearch.java

--- java/BinarySearch.java
+++ java/BinarySearch.java
@@ -1,43 +1,47 @@
-import java.util.Arrays;
+/******************************************************************************
 
-public class BinarySearch
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+import java.util.Arrays;
+import java.util.*;
+
+public class Main
 {
-    public static int[] arr = {10,30,60,40,55,90,80};
+    public static int[] arr = {4,8,10,15,21,24,27};
     
-    public static void printArray() {
-        for(int i=0;i<arr.length;i++) {
-            System.out.print(arr[i]+ " ");
-        }
-        System.out.println();
-    }
+    public static Scanner scanner = new Scanner(System.in);
     
-    
-    public static int binarySearchFunc(int l, int h, int key) {
+    public static int binarySearch(int[] arr, int l, int h, int key) {
         while(l<=h) {
             int mid = (l+h)/2;
             if(arr[mid] == key) return mid;
-            else if(key < arr[mid]) {
-                h = mid-1;
+            else if( key < arr[mid]) {
+                h = mid - 1;
             }
-            else l = mid+1;
+            
+            else l = mid + 1;
         }
+        
         return -1;
     }
     
-	public static void main(String[] args) {
-	    
-	    printArray();
-	    
-	    // sort the Array 
-	    Arrays.sort(arr);
-	    
-	    System.out.println("array after sort: ");
-	    printArray();
-	    
-	    // binary function 
-	    int result = binarySearchFunc(0, arr.length, 60);
-	    if(result!=-1) System.out.println("element found at location "+result);
-	    else System.out.println("element found not found");
-
-	}
+    
+    public static void main(String[] args) {
+        Arrays.sort(arr);
+        
+        System.out.println("enter key to search: ");
+        int key = scanner.nextInt();
+        
+        if(binarySearch(arr, 0, arr.length-1, key) == -1) {
+            System.out.println("not found!");
+        }   
+        
+        else System.out.println("found!");
+        
+    }
 }
