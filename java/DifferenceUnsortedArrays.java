3f313eb876b5e0edbda9986b36ff1654412ab642
Mayur Aitavadekar <40314912+mayuraitavadekar@users.noreply.github.com>
2021-04-07 18:42:59 +0530

    Create DifferenceUnsortedArrays.java

---
+++ java/DifferenceUnsortedArrays.java
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+A - B = copy those elements which are only in A. 
+
+*******************************************************************************/
+
+import java.util.Arrays;
+
+public class Main
+{
+    public static int[] A = {3,5,10,4,6};
+	public static int[] B = {12,4,7,2,5};
+	public static int[] C = new int[A.length + B.length];
+    
+	public static void main(String[] args) {
+	       
+	   int flag = 0;
+	   int k = 0;
+	   for(int i=0;i<A.length;i++) {
+	       flag = 0;
+	       for(int j=0;j<B.length;j++) {
+	           if(A[i] == B[j]) {
+	               flag =  1;
+	           }
+	       }
+	       
+	       if(flag == 0) {
+	           C[k++] = A[i]; // in A - B we always copy from A
+	       }
+	   }
+	   
+	   System.out.println("A - B");
+	   
+	   for(int i=0;i<C.length;i++) {
+	       System.out.print(C[i] + " ");
+	   }
+	    
+	}
+}
