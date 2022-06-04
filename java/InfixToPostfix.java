9f7b5094d314e9887de4368b3681d3c5fecfa19a
Mayur Aitavadekar <40314912+mayuraitavadekar@users.noreply.github.com>
2022-06-04 17:50:38 -0400

    Update InfixToPostfix.java

--- java/InfixToPostfix.java
+++ java/InfixToPostfix.java
@@ -2,80 +2,81 @@ package Leetcode;
 
 import java.util.*;
 
-public class InfixToPostfix {
-	
-	public static int precedence(char x)
-	{
-		if(x == '+' || x == '-')
-		{
-			return 1;
-		}
-		else if(x == '*' || x == '/')
-		{
-			return 2;
-		}
-		
-		return 0;
-	}
-	
-	public static String infixToPostfix(String exp)
-	{
-		String result =  new String("");
-		Stack<Character> stack = new Stack<>();
-		
-		for(int i=0;i<exp.length();i++)
-		{
-			char c = exp.charAt(i);
-			
-			if(Character.isLetterOrDigit(c))
-			{
-				result = result + c;
-			}
-			else if(c == 'c')
-			{
-				stack.push(c);
-			}
-			else if(c == ')')
-			{
-				while(!stack.empty() || stack.peek() == '(')
-				{
-					result += stack.pop();
-				}
-				
-				if(!stack.empty() || stack.peek() == '(')
-				{
-					return "invalid expression";
-				}
-				
-				else
-				{
-					stack.pop();
-				}
-			}
-			
-			else 
-			{
-				while(!stack.isEmpty() && precedence(c) <= precedence(stack.peek()))
-				{
-                    			result += stack.pop();
-				}
-				// if stack is empty or precedence of current is greater then push into stack
-                		stack.push(c);
-			}
-		}
-		
-		// finally whatever in the stack add to result
-		while (!stack.isEmpty())
-		{	
-            		result += stack.pop();
-		}
+class InfixToPostfix
+{
+    // A utility function to return precedence of a given operator
+    // Higher returned value means higher precedence
+    static int Prec(char ch)
+    {
+        switch (ch)
+        {
+            case '+':
+            case '-':
+                return 1;
 
-        	return result;
-	}
-	
-	public static void main(String[] args)
-	{
-		String exp = "a+b*c-d/e";
-		System.out.println(infixToPostfix(exp));
-	}
+            case '*':
+            case '/':
+                return 2;
+
+            case '^':
+                return 3;
+        }
+        return -1;
+    }
+
+
+    static String infixToPostfix(String exp)
+    {
+        // initializing empty String for result
+        String result = new String("");
+
+        // initializing empty stack
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i<exp.length(); ++i)
+        {
+            char c = exp.charAt(i);
+
+            // If the scanned character is an operand, add it to output.
+            if (Character.isLetterOrDigit(c))
+                result += c;
+
+                // If the scanned character is an '(', push it to the stack.
+            else if (c == '(')
+                stack.push(c);
+
+                //  If the scanned character is an ')', pop and output from the stack
+                // until an '(' is encountered.
+            else if (c == ')')
+            {
+                while (!stack.isEmpty() && stack.peek() != '(')
+                    result += stack.pop();
+
+                if (!stack.isEmpty() && stack.peek() != '(')
+                    return "Invalid Expression"; // invalid expression
+                else
+                    stack.pop();
+            }
+            else // an operator is encountered
+            {
+                while (!stack.isEmpty() && Prec(c) <= Prec(stack.peek()))
+                    result += stack.pop();
+                stack.push(c);
+            }
+
+        }
+
+        // pop all the operators from the stack
+        while (!stack.isEmpty())
+            result += stack.pop();
+
+        return result;
+    }
+
+    // Driver method
+    public static void main(String[] args)
+    {
+        String exp = "(a+b)*c";
+        System.out.println(infixToPostfix(exp));
+    }
 }
