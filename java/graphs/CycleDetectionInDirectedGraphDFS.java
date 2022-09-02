90680d541edeb35fc19fd7695bcab8309fdd6493
Mayur Aitavadekar <40314912+mayuraitavadekar@users.noreply.github.com>
2022-09-01 20:35:01 -0400

    Update CycleDetectionInDirectedGraphDFS.java

--- java/graphs/CycleDetectionInDirectedGraphDFS.java
+++ java/graphs/CycleDetectionInDirectedGraphDFS.java
@@ -1,102 +1,95 @@
-package Leetcode;
-
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-
-public class CycleDetectionInDirectedGraphDFS {
-	
-	public static HashMap<Integer, List<Integer>> map = new HashMap<>();
-	public static Map<Integer, Boolean> dfs_visited = new HashMap<>();
-	public static Map<Integer, Boolean> visited = new HashMap<>();
-	
-	public void addEdge(int u, int v)
-	{
-		if(!map.containsKey(u))
-		{
-			map.put(u, new ArrayList<Integer>());
-		}
-		
-		if(!map.containsKey(v))
-		{
-			map.put(v, new ArrayList<Integer>());
-		}
-
-		map.get(u).add(v);
-	}
-	
-	public boolean cycleDetectionInDirectedGraph(
-			int source, 
-			Map<Integer, Boolean> visited,
-			Map<Integer, Boolean> dfs_visited
-			)
-	{
-		visited.put(source, true);
-		dfs_visited.put(source, true);
-		
-		Iterator<Integer> i = map.get(source).iterator();
-		while(i.hasNext())
-		{
-			int node = i.next();
-			if(visited.get(node) != true)
-			{	
-				boolean res = cycleDetectionInDirectedGraph(node, visited, dfs_visited);
-				if(res == true)
-				{
-					return true;
-				}
-			}
-			else if(visited.get(node) == true && dfs_visited.get(node) == true)
-			{
-				return true;
-			}
-		}
-		
-		dfs_visited.put(source, false);
-		return false;
-		
-	}
-	
-	public static void main(String[] args)
-	{
-		CycleDetectionInDirectedGraphDFS obj = new CycleDetectionInDirectedGraphDFS();
-		
-		obj.addEdge(2, 0);
-		obj.addEdge(1, 0);
-		obj.addEdge(3, 1);
-		obj.addEdge(3, 2);
-		obj.addEdge(1, 3);
-//		obj.addEdge(2, 4);
-//		obj.addEdge(4, 5);
-//		obj.addEdge(5, 6);
-//		obj.addEdge(6, 4);
-//		obj.addEdge(8, 7);
-		
-		for(Map.Entry<Integer, List<Integer>> m : map.entrySet())
-		{
-			dfs_visited.put(m.getKey(), false);
-			visited.put(m.getKey(), false);
-		}
-		
-		int arr[] = {2,0,1,3,1};
-		for(int i=0;i<arr.length;i++)
-		{
-			if(visited.get(arr[i]) != true)
-			{
-				boolean res = obj.cycleDetectionInDirectedGraph(arr[i], visited, dfs_visited);
-				if(res == true)
-				{
-					System.out.println("yes cycle is present");
-				}
-			}
-		}
-		
-		System.out.println("no cycle is not present");
-		
-//		boolean res = obj.cycleDetectionInDirectedGraph(2, visited, dfs_visited);
-//		if(res == true) System.out.println("yes cycle is present");
-//		else System.out.println("no cycle is not present");
-	}
+import java.util.*;
+public class Solution {
+  public static boolean detectCycleInDirectedGraph(int n, ArrayList < ArrayList < Integer >> edges) {
+    // Write your code here.
+      Map<Integer, List<Integer>> map = new HashMap<>();
+      Map<Integer, Boolean> visited = new HashMap<>();
+      Map<Integer, Boolean> dfs_visited = new HashMap<>();
+      
+      for(int i=1;i<=n;i++)
+      {
+          visited.put(i, false);
+          dfs_visited.put(i, false);
+      }
+      
+      // build graph
+      for(int i=0;i<edges.size();i++)
+      {
+          int u = edges.get(i).get(0);
+          int v = edges.get(i).get(0);
+          
+          buildGraph(u, v, map);
+      }
+      
+      // cycle detection using DFS
+      for(int i=1;i<=n;i++)
+      {
+          if(visited.get(i) != true)
+          {
+              // visit this
+              if(dfs(i, map, visited, dfs_visited))
+              {
+                  return true;
+              }
+          }
+      }
+      
+      return false;
+  }
+    
+    public static boolean dfs(int source, Map<Integer, List<Integer>> map, Map<Integer, Boolean> visited,  Map<Integer, Boolean> dfs_visited)
+    {
+        // visit the source
+        visited.put(source, true);
+        dfs_visited.put(source, true);
+        
+        if(map.get(source) != null)
+        {
+            Iterator<Integer> i = map.get(source).iterator();
+            
+            while(i.hasNext())
+            {
+                // get the node from list
+                int node = i.next();
+                
+                // check if it is visited
+                if(visited.get(node) != true)
+                {
+                    // this node is not visited
+                    boolean res = dfs(node, map, visited, dfs_visited);
+                    if(res == true)
+                    {
+                        // cycle is present
+                        return true;
+                    }
+                }
+                
+                else if(visited.get(node) == true && dfs_visited.get(node) == true)
+                {
+                    return true;
+                }
+            }
+        }
+        
+        dfs_visited.put(source, false);
+        return false;
+    }
+  
+    public static void buildGraph(int u, int v, Map<Integer, List<Integer>> map)
+    {
+        if(!map.containsKey(u))
+        {
+            map.put(u, new ArrayList<>());
+        }
+        
+        if(!map.containsKey(v))
+        {
+            map.put(v, new ArrayList<>());
+        }
+        
+        // graph is directed
+        map.get(u).add(v); // u -> [v]
+    }
+  
 }
